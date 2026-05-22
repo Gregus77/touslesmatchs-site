93b0c263ae34355f7798aa6d613c251538299153
Gregus77 <129583179+Gregus77@users.noreply.github.com>
2026-05-22 15:29:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ var NETBET_LINK = "https://www.netbet.fr/affiliation-TONLIEN";
 var TIKTOK_LINK = "https://www.tiktok.com/@touslesmatchs.com";
 
 var picks = [
-  ["22/05","New York Knicks vs Cleveland Cavaliers","Plus de 215.5 pts","1.87","---","EN ATTENTE","Basketball"],
+  ["22/05","New York Knicks vs Cleveland Cavaliers","Plus de 215.5 pts","1.87","109-93 (202 pts)","PERDU","Basketball"],
   ["20/05","Fribourg vs Aston Villa","Victoire Aston Villa","1.58","0-1","GAGNE","Foot"],
   ["19/05","New York Knicks vs Cleveland Cavaliers","Plus de 216.5 pts","1.85","115-104 (219 pts)","GAGNE","Basketball"],
   ["19/05","Boca Juniors vs Cruzeiro","Moins de 2.5 buts","1.40","1-1","GAGNE","Foot"],
